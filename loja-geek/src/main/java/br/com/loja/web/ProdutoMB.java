3236702e022171ab52714b5a5be1d12cb3866832
HeloisaGalvao <33168737+HeloisaGalvao@users.noreply.github.com>
2018-11-17 15:43:46 -0200

    Add files via upload
    MB de produto.xhtml

---
+++ loja-geek/src/main/java/br/com/loja/web/ProdutoMB.java
@@ -0,0 +1,73 @@
+package br.com.loja.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.Application;
+import javax.faces.application.ViewHandler;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+import br.com.loja.dao.ProdutoDAO;
+import br.com.loja.modelos.Produto;
+
+@ManagedBean
+@SessionScoped
+public class ProdutoMB {
+
+	public ProdutoMB() {
+		ProdutoDAO pd = new ProdutoDAO();
+		this.lista = pd.listarProdutos();
+		System.out.println("listou");
+		/*
+		for (Produto produto : lista) {
+			System.out.println(produto.getIdProduto());
+			System.out.println(produto.getDescricao());
+			System.out.println(produto.getPreco());
+			System.out.println(produto.getModelo());
+			System.out.println(produto.getTamanho());
+		}*/
+	}
+	Produto produto = new Produto();
+	private List<Produto> lista = new ArrayList<Produto>();
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public List<Produto> getLista() {
+		return lista;
+
+	}
+
+	public void setLista(List<Produto> lista) {
+		this.lista = lista;
+	}
+	
+	public void removerItem(Produto produto) {
+	/*	System.out.println("achou");
+		lista.remove(id);
+		System.out.println("removeu");*/
+		ProdutoDAO pd = new ProdutoDAO();
+		pd.excluirPorObjeto(produto);
+		this.refresh();
+		
+	}
+
+	public void refresh() {
+		System.out.println("refresh");
+		// teste para atualizar página (NÃO FUNCIONOU)
+		FacesContext context = FacesContext.getCurrentInstance();
+		Application application = context.getApplication();
+		ViewHandler viewHandler = application.getViewHandler();
+		UIViewRoot viewRoot = viewHandler.createView(context, context.getViewRoot().getViewId());
+		context.setViewRoot(viewRoot);
+		context.renderResponse();
+	}
+}
