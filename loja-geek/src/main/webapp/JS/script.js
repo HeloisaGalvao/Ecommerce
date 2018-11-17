a7d573708d8ffd90cbc2061ebfa06971f4d8e1d7
Matheus Rhumenig <mrfmatheus@hotmail.com>
2018-11-17 03:19:22 -0300

    Agr ta funcionando esse carai

--- loja-geek/src/main/webapp/JS/script.js
+++ loja-geek/src/main/webapp/JS/script.js
@@ -1,27 +1,37 @@
 
  //FAZ ISSO FUNCIONAR PF !!!! 
    //Setar no localStorage como array
-$(function(){
-    var setLocalStorage = function(obj){
-        if(!localStorage.getItem('obj')){
-            var arrayObject = [];
-            arrayObject.push(obj);
-            localStorage.setItem('obj',JSON.stringify(arrayObject));
-        }else{
-            var objeto = JSON.parse(localStorage.getItem('obj'));
-            objeto.push(obj);
-            localStorage.setItem('obj',JSON.stringify(objeto));
-        }
-    }
-	});
+//$(function(){
+//    var setLocalStorage = function(obj){
+//        if(!localStorage.getItem('obj')){
+//            var arrayObject = [];
+//            arrayObject.push(obj);
+//            localStorage.setItem('obj',JSON.stringify(arrayObject));
+//        }else{
+//            var objeto = JSON.parse(localStorage.getItem('obj'));
+//            objeto.push(obj);
+//            localStorage.setItem('obj',JSON.stringify(objeto));
+//        }
+//    }
+//	});
 
 $(function(){
 	$("#Add").on("click", function(){
 		var title= $(this).closest(".item").find(".card-title a").text();
 		var price= $(this).closest(".item").find("h5").text();
-
 	var obj={titulo:title,preco:price};
+	
+	if(!localStorage.getItem('obj')){
+        var arrayObject = [];
+        arrayObject.push(obj);
+        localStorage.setItem('obj',JSON.stringify(arrayObject));
+    }
+	else{
+        var objeto = JSON.parse(localStorage.getItem('obj'));
+        objeto.push(obj);
+        localStorage.setItem('obj',JSON.stringify(objeto));
+    }
 	console.log(obj); 
-	localStorage.setItem('obj',JSON.stringify(obj))
+//localStorage.setItem('obj',JSON.stringify(obj))
 	});	
 });
